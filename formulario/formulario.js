e71d9595d3d8729eac14c563c38a70817a01db3f
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2024-07-08 07:45:39 -0300

    Add files via upload

---
+++ formulario/formulario.js
@@ -0,0 +1,16 @@
+function enviar() {
+
+    let nomeUsuario = document.getElementById('nome').value
+    let emailUsuario = document.getElementById('email').value
+    let data = document.getElementById('data').value
+    let senha = document.getElementById('senha').value
+
+    let classeViagem = document.querySelector("input[name='classe']:checked").value
+
+    alert('Seu nome é: ' + nomeUsuario + '\n' 
+    + 'Seu email é: ' + emailUsuario + '\n'
+    + 'Data de nascimento: ' + data + '\n'
+    + 'Sua senha registrada foi: ' + senha + '\n'
+    + 'Você escolheu viajar na: ' + classeViagem)
+    
+}
